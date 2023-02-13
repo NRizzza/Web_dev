7a8314e9238cd40982e56ca45d514036e5dfc8bc
Riza <98638032+VernoKira@users.noreply.github.com>
2023-02-14 00:35:37 +0600

    Update script.js
    добавление новых задач, клик по кнопке

--- lab3/toDo_list/script.js
+++ lab3/toDo_list/script.js
@@ -3,4 +3,29 @@ const dom = {
     add: document.getElementById('add'),
     tasks: document.getElementById('tasks')
 }
+
+const tasks = [];
 // console.log(dom)
+// добавляем задачу 
+// клик по кнопке Добавить задачу
+
+dom.add.onclick = () =>{
+    const task = dom.new.value
+    if(task){ /*add is not empty*/ 
+        addTask()
+    }
+}
+
+// function to add task
+
+function addTask(text){
+    const timestamp = Date.now();
+    const task ={
+        id: timestamp,
+        text,
+        isFinished: false
+    }
+    tasks.push(task)
+
+    
+}
