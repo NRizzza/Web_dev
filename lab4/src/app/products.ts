5d504d07f5027a6d89e34b7b2c497e742a9501f2
Riza <98638032+VernoKira@users.noreply.github.com>
2023-02-22 15:51:41 +0600

    Update products.ts
    +5 products

--- lab4/src/app/products.ts
+++ lab4/src/app/products.ts
@@ -42,7 +42,8 @@ export const products = [
     price: 54582,
     description: 'Yves Saint Laurent Black Opium парфюмерная вода EDP 50 мл',
     image: 'assets/ph/black.jpg',
-    rating:5
+    rating:5,
+    url:"https://kaspi.kz/shop/p/yves-saint-laurent-black-opium-parfjumernaja-voda-edp-50-ml-17301512/?c=750000000#!/item"
   },
   {
     id: 5,
@@ -52,13 +53,50 @@ export const products = [
     image: 'assets/ph/montale.jpg',
     rating:5,
     url:"https://kaspi.kz/shop/p/montale-intense-cafe-parfjumernaja-voda-edp-100-ml-17300132/?c=750000000#!/item"
+  },
+  {
+    id: 6,
+    name: 'Black Dress',
+    price: 21945,
+    description: 'Guerlain La Petite Robe Noire Ma Robe Cocktail туалетная вода EDT 30 мл',
+    image: 'assets/ph/dress.jpg',
+    rating:4.65,
+    url:"https://kaspi.kz/shop/p/guerlain-la-petite-robe-noire-ma-robe-cocktail-tualetnaja-voda-edt-30-ml-100868479/?c=750000000#!/item"
+  },
+  {
+    id: 7,
+    name: 'Lancome',
+    price: 51980,
+    description: 'LANCOME La Vie Est Belle парфюмерная вода EDP 30 мл',
+    image: 'assets/ph/lancome.jpg',
+    rating:4.12,
+    url:"https://kaspi.kz/shop/p/lancome-la-vie-est-belle-parfjumernaja-voda-edp-30-ml-17301543/?c=750000000#!/item"
+  },
+  {
+    id: 8,
+    name: 'Molecule',
+    price: 47266,
+    description: 'Escentric Molecules Molecule 02 туалетная вода EDT 100 мл',
+    image: 'assets/ph/molecule.jpg',
+    rating:3.98,
+    url:"https://kaspi.kz/shop/p/escentric-molecules-molecule-02-tualetnaja-voda-edt-100-ml-17300351/?c=750000000#!/item"
+  },
+  {
+    id: 9,
+    name: 'Kirke',
+    price: 60963,
+    description: 'Tiziana Terenzi Kirke парфюмерная вода EDP 100 мл',
+    image: 'assets/ph/kirke.jpg',
+    rating:5,
+    url:"https://kaspi.kz/shop/p/tiziana-terenzi-kirke-parfjumernaja-voda-edp-100-ml-17301541/?c=750000000#!/item"
+  },
+  {
+    id: 10,
+    name: 'Lost Cherry',
+    price: 163990,
+    description: 'TOM FORD Lost Cherry парфюмерная вода EDP 50 мл',
+    image: 'assets/ph/cherry.jpg',
+    rating:4.81,
+    url:"https://kaspi.kz/shop/p/tom-ford-lost-cherry-parfjumernaja-voda-edp-50-ml-17302405/?c=750000000#!/item"
   }
- 
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
