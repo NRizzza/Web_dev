61eeb0e345f84b7def232639c2b6f08fccd9c7ed
Riza <98638032+VernoKira@users.noreply.github.com>
2023-03-01 00:28:07 +0600

    Update product-list.component.ts
    функции для удаления и лайков

--- lab4/src/app/product-list/product-list.component.ts
+++ lab4/src/app/product-list/product-list.component.ts
@@ -10,12 +10,81 @@ import { products } from '../products';
 export class ProductListComponent {
 
   products = [...products];
+  filteredProducts = [...products]
+  pickedCategory ='None'
 
   share(name:string, url:string) {
-    // window.alert('The product has been shared!');
-    window.open(`https://telegram.me/share/url?url=Welcome, you should buy ${name} &text= on ${url}`)
+    window.open(`https://telegram.me/share/url?url=Welcome, купи Ризе ${name} &text= по ссылке ${url}`)
    
   }
+  filterList(){
+    if(this.pickedCategory == 'None'){
+      this.filteredProducts = this.products;
+    }
+    else{
+      this.filteredProducts = this.products.filter(p  => p.category === this.pickedCategory);
+    }
+  }
 
- 
+  updateFilter(cat: string){
+    this.pickedCategory = cat;
+    this.filterList();
+  }
+  
+  increaseLikes(name: string){
+    this.products.filter(p => p.name == name).map(p => p.likes++);
+  }
+
+  deleteItem(id: number){
+    this.products = this.products.filter(p => p.id != id);
+    this.filterList();
+  }
 }
+
+
+
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+
+  products = [...products];
+  filteredProducts = [...products]
+  pickedCategory ='None'
+
+  share(name:string, url:string) {
+    window.open(`https://telegram.me/share/url?url=Welcome, купи Ризе ${name} &text= по ссылке ${url}`)
+   
+  }
+  filterList(){
+    if(this.pickedCategory == 'None'){
+      this.filteredProducts = this.products;
+    }
+    else{
+      this.filteredProducts = this.products.filter(p  => p.category === this.pickedCategory);
+    }
+  }
+
+  updateFilter(cat: string){
+    this.pickedCategory = cat;
+    this.filterList();
+  }
+  
+  increaseLikes(name: string){
+    this.products.filter(p => p.name == name).map(p => p.likes++);
+  }
+
+  deleteItem(id: number){
+    this.products = this.products.filter(p => p.id != id);
+    this.filterList();
+  }
+}
+
+
+
