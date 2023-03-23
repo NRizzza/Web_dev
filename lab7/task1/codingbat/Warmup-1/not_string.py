b9eff400eb1f59fffffc01bb6d7faf5369a52832
Riza <98638032+VernoKira@users.noreply.github.com>
2023-03-23 15:59:42 +0600

    Add files via upload

---
+++ lab7/task1/codingbat/Warmup-1/not_string.py
@@ -0,0 +1,14 @@
+# Given a string, return a new string where "not " has been added to the front.
+# However, if the string already begins with "not", return the string unchanged.
+
+
+# not_string('candy') → 'not candy'
+# not_string('x') → 'not x'
+# not_string('not bad') → 'not bad'
+
+def not_string(str):
+    if not str.startswith("not"):
+        return "not " + str
+    else:
+        return str
+
