6974d4d0ce98e074b91f72d620cfc4ac7016c1d5
Riza <98638032+VernoKira@users.noreply.github.com>
2023-04-05 13:49:33 +0600

    Update views.py
    создаем вьюс

--- lab8/shopback/api/views.py
+++ lab8/shopback/api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from .models import Product, Category
 
 # Create your views here.
+def category_list(request):
+    categories = Category.objects.all()
+    categories_json=[category.to_json() for category in categories]
+    return JsonResponse(categories_json, safe=False)
+
+def category_detail(request, id):
+    try:
+        category = Category.objects.get(id=id)
+    except Category.DoesNotExist as e:
+        return JsonResponse({'message': str(e)}, status=400)
+    return JsonResponse(category.to_json())
+
+def product_list(request):
+    products = Product.objects.all()
+    products_json = [product.to_json() for product in products]
+    return JsonResponse(products_json, safe=False)
+
+def product_detail(request, id):
+    try:
+        product = Product.objects.get(id=id)
+    except Product.DoesNotExist as e:
+        return JsonResponse({'message': str(e)}, status=400)
+    return JsonResponse(product.to_json())
+
+def category_products(request, id):
+    category = Category.objects.get(id=id)
+    products = Product.objects.filter(category=category)
+    products_json = [product.to_json() for product in products]
+    return JsonResponse(products_json, safe=False)
