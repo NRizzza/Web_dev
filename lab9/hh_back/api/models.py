87f3dcb1929018f2da4a263a89352d711bd85ed4
Riza <98638032+VernoKira@users.noreply.github.com>
2023-04-07 16:17:16 +0600

    Add files via upload

---
+++ lab9/hh_back/api/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(default='')
+    city = models.CharField(max_length=255)
+    address = models.TextField(max_length=255)
+
+    def str(self):
+        return f'{self.id}, {self.name}'
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'city': self.city,
+            'address': self.address
+        }
+
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(default='')
+    salary = models.FloatField(default=0)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def str(self):
+        return f'{self.id}, {self.name}'
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary,
+            'company': self.company.to_json()
+        }
