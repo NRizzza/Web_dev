350faba6915fce7315470ae8a7f0bb485ee2f0a5
Riza <98638032+VernoKira@users.noreply.github.com>
2023-04-19 00:05:31 +0600

    Update app-routing.module.ts

--- lab10/hh_front/src/app/app-routing.module.ts
+++ lab10/hh_front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CompanyComponent} from "./company/company.component";
+import {VacancyComponent} from "./vacancy/vacancy.component";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CompanyComponent },
+  { path: 'companies', component: CompanyComponent },
+  { path: 'companies/:id/vacancies', component: VacancyComponent },
+  { path: '', redirectTo: 'companies', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
